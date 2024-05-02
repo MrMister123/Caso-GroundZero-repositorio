365e37d8dac57553142fbecabb56b5429f3bf4be
dan <137331255+MrMister123@users.noreply.github.com>
2024-05-02 11:48:30 -0400

    Add files via upload

--- VALIDACION.js
+++ VALIDACION.js
@@ -1,16 +1,30 @@
 var user=document.getElementById('user');
+var correo=document.getElementById('correo');
 var password=document.getElementById('password');
-var error=document.getElementById('error');
+error.style.color='red';
 
-var form=document.getElementById('form');
+var form=document.getElementById('formulario');
 form.addEventListener('submit', function(evt){
     evt.preventDefault();
     console.log('Enviando formulario...')
     var mensajesError=[];
 
     if(user.value===null || user.value===''){
-        mensajesError.push('Ingresa tu user');
+        mensajesError.push('Ingresa tu nombre de usuario');
+    }else{
+        if(user.value.length<3 || user.value.length>20){
+            mensajesError.push('El largo de tu nombre de usuario debe estar entre 3 y 20 caracteres');
+        }
     }
 
+    if(correo.value===null || correo.value===''){
+        mensajesError.push('Ingresa tu correo electronico')
+    }
+
+    if(password.value===null || password.value===''){
+            mensajesError.push('Ingresa tu contraseña');
+    }
+
+   
     error.innerHTML = mensajesError.join(', ');
 });
