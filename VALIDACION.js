705661f36b43d17ff04998983da0dad280aef8c1
dan <137331255+MrMister123@users.noreply.github.com>
2024-05-12 20:41:32 -0400

    Add files via upload

--- VALIDACION.js
+++ VALIDACION.js
@@ -1,4 +1,4 @@
-var user=document.getElementById('user');
+/*var user=document.getElementById('user');
 var correo=document.getElementById('correo');
 var password=document.getElementById('password');
 error.style.color='red';
@@ -27,4 +27,35 @@ form.addEventListener('submit', function(evt){
 
    
     error.innerHTML = mensajesError.join(', ');
+});*/
+
+const user = document.getElementById('user')
+const correo = document.getElementById('correo')
+const pass = document.getElementById('password')
+const form = document.getElementById('formulario')
+const parrafo = document.getElementById('error')
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    parrafo.innerHTML = ""
+    if(user.value.length < 6){
+        warnings += `El nombre no es valido <br>` 
+        entrar = true
+    }
+    if(!regexEmail.test(correo.value)){
+        warnings += `El correo no es valido <br>` 
+        entrar = true
+    }
+    if (pass.value.length < 8){
+        warnings += `La contraseña no es valido <br>` 
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = `form validado`
+    }
 });
