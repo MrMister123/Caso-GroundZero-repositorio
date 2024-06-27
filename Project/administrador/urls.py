3488500072d34eb21a816430d758be3fe382a215
dan <137331255+MrMister123@users.noreply.github.com>
2024-06-26 22:06:41 -0400

    Add files via upload

--- Project/administrador/urls.py
+++ Project/administrador/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'administrador'
 urlpatterns = [
     path('crudMenu', views.menuCrud, name = 'crudMenu'),
-    path('home', views.home, name = 'home')
-
+    path('home', views.home, name = 'home'),
 ]
--- Project/administrador/views.py
+++ Project/administrador/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from usuarios.models import Usuario
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 @login_required
 def menuCrud(request):
