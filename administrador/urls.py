85457ed6cde87c10f576d13bda6ea9c2c549bfd7
dan <137331255+MrMister123@users.noreply.github.com>
2024-07-09 22:22:52 -0400

    Delete administrador directory

--- administrador/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'administrador'
-urlpatterns = [
-    path('crudMenu', views.menuCrud, name = 'crudMenu'),
-    path('home', views.home, name = 'home'),
-]
