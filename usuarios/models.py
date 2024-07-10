c94082646a54e5abd0fb0123e74f5cef9ba7fa1c
dan <137331255+MrMister123@users.noreply.github.com>
2024-07-09 22:23:24 -0400

    Delete usuarios directory

--- usuarios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    rut = models.CharField(primary_key=True, max_length=10)
-    nombre = models.CharField(max_length=20)
-    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
-    activo = models.IntegerField()
-def __str__(self):
-    return str(self.nombre)
